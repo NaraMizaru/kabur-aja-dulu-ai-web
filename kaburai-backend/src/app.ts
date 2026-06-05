aaa829c9860d24c46102ba21e9bd30182ea038b8
m1zaru <hilal.muhammad0807@gmail.com>
2026-06-05 11:13:21 +0700

    Format app.ts and update package-lock.json Format app.ts and add Docker config and lockfile

--- kaburai-backend/src/app.ts
+++ kaburai-backend/src/app.ts
@@ -5,8 +5,8 @@ import router from "./routes";
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api', router)
+app.use('/api', router);
 
 export default app;
